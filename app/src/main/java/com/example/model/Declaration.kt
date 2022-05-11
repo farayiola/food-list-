631ff5fe5bc13679ae85a617e11cb42ebddf7ef1
farayiola <afolabiabolaji2015@gmail.com>
2022-05-11 01:53:24 -0700

    kodecamp

---
+++ app/src/main/java/com/example/model/Declaration.kt
@@ -0,0 +1,7 @@
+package com.example.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+
+data class Declaration(@StringRes val StringResourceId: Int, val StringResourceid: Int, @DrawableRes val imageResourceId: Int)
